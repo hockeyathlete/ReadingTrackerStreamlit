d16bfead12304c377ab925de42779a8150ad460d
Alan <alan.leeser@gmail.com>
2023-12-31 12:27:20 -0600

    Add backend data to session state Add loading status to main page Add data preview once data is loaded to main page

--- backend.py
+++ backend.py
@@ -2,20 +2,24 @@ import pandas as pd
 import numpy as np
 from datetime import timedelta, date
 
-# Load Data
-sheet_id = '1lQQliuMCJPLcfn3CzaK4aU6-Ro3wkMJ1Ghqa8vdJ1ho'
-book_tracker = 'Book%20Tracker'
-book_tracker_url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={book_tracker}'
-reading_log = 'Reading%20Log'
-reading_log_url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={reading_log}'
-books = pd.read_csv(book_tracker_url)
-log = pd.read_csv(reading_log_url)
-ebook_normalizer = 0.89
 
-# Book Data
-books['Start Date'] = pd.to_datetime(books['Start Date'])
-books['Finish Date'] = pd.to_datetime(books['Finish Date'])
+def load_data():
+    # Load Data
+    sheet_id = '1lQQliuMCJPLcfn3CzaK4aU6-Ro3wkMJ1Ghqa8vdJ1ho'
+    book_tracker = 'Book%20Tracker'
+    book_tracker_url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={book_tracker}'
+    reading_log = 'Reading%20Log'
+    reading_log_url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={reading_log}'
+    books = pd.read_csv(book_tracker_url)
+    log = pd.read_csv(reading_log_url)
+    ebook_normalizer = 0.89
 
-books.loc[books['Format'] == 'Book', 'pages_norm'] = books['Pages']
-books.loc[books['Format'] == 'eBook', 'pages_norm'] = round(books['Pages']*ebook_normalizer)
-books.loc[books['Format'] == 'Audiobook', 'pages_norm'] = 0
+    # Book Data
+    books['Start Date'] = pd.to_datetime(books['Start Date'])
+    books['Finish Date'] = pd.to_datetime(books['Finish Date'])
+
+    books.loc[books['Format'] == 'Book', 'pages_norm'] = books['Pages']
+    books.loc[books['Format'] == 'eBook', 'pages_norm'] = round(books['Pages'] * ebook_normalizer)
+    books.loc[books['Format'] == 'Audiobook', 'pages_norm'] = 0
+
+    return books, log
--- main.py
+++ main.py
@@ -4,4 +4,13 @@ import backend
 
 
 st.title('Book Tracker')
-st.write(backend.books)
+
+if "book_data" not in st.session_state:
+    status = st.text('Loading Data')
+    st.session_state.book_data, st.session_state.log_data = backend.load_data()  # 👈 Download the data
+
+st.header('Book Data')
+st.write(st.session_state.book_data)
+
+st.header('Log Data')
+st.write(st.session_state.log_data)
