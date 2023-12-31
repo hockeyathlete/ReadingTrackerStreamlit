fbdf6db0093c585fc8178303a35c071e2f8c6362
Alan <alan.leeser@gmail.com>
2023-12-31 15:33:02 -0600

    Add days of the week chart

--- pages/page_stats.py
+++ pages/page_stats.py
@@ -16,6 +16,20 @@ def pages_per_day_chart(pages_per_day_data):
     return chart
 
 
+def day_of_week_chart(day_of_week_data):
+    weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    base = alt.Chart(day_of_week_data).mark_bar().encode(
+        x=alt.X('weekday', title=None, sort=weekdays),
+    )
+
+    chart = alt.hconcat(
+        base.encode(y='Pages Read').properties(title='Avg Pages/Day'),
+        base.encode(y=alt.Y('pages_norm', title='Pages Read')).properties(title='Avg Pages/Day (Normalized)')
+    )
+    # chart = base.encode(y='Pages Read')
+    return chart
+
+
 st.title('Page Stats')
 with st.expander('How pages are counted across different mediums'):
     st.text('Normalized data means eBooks pages are multiplied by a factor of 0.89 to account \nfor shorter pages')
@@ -26,7 +40,17 @@ years = np.sort(st.session_state.log_data['year'].unique())
 year = st.selectbox('Pick a year to review', [int(year) for year in years][::-1])
 
 pages_per_day = st.session_state.log_data[st.session_state.log_data['year'] == year].groupby('Date').agg(
-    {'Pages Read':'sum', 'pages_norm':'sum', 'month_year':'first'}).groupby('month_year')[['Pages Read', 'pages_norm']]\
+    {'Pages Read': 'sum', 'pages_norm': 'sum', 'month_year': 'first'}).groupby('month_year')[
+    ['Pages Read', 'pages_norm']] \
     .mean().reset_index()
 
 st.altair_chart(pages_per_day_chart(pages_per_day))
+
+st.header('Days of the Week')
+
+
+day_of_week = st.session_state.log_data.groupby('Date').agg(
+    {'Pages Read': 'sum', 'pages_norm': 'sum', 'weekday': 'first'}).groupby('weekday')[['Pages Read', 'pages_norm']] \
+    .mean().reset_index()
+
+st.altair_chart(day_of_week_chart(day_of_week))
