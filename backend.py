07d0d969ba052e2beeb400d34f6f58f85871dba3
Alan <alan.leeser@gmail.com>
2023-12-31 15:26:03 -0600

    Add preprocessing for log data

--- backend.py
+++ backend.py
@@ -14,7 +14,7 @@ def load_data():
     log = pd.read_csv(reading_log_url)
     ebook_normalizer = 0.89
 
-    # Book Data
+    # Preprocess Book Data
     books['Start Date'] = pd.to_datetime(books['Start Date'])
     books['Finish Date'] = pd.to_datetime(books['Finish Date'])
     books.loc[~books['Finish Date'].isnull(), 'finish_year'] = pd.to_datetime(books['Finish Date']).dt.year
@@ -22,5 +22,15 @@ def load_data():
     books.loc[books['Format'] == 'Book', 'pages_norm'] = books['Pages']
     books.loc[books['Format'] == 'eBook', 'pages_norm'] = round(books['Pages'] * ebook_normalizer)
     books.loc[books['Format'] == 'Audiobook', 'pages_norm'] = 0
+
+    # Preprocess Log Data
+    log['Date'] = pd.to_datetime(log['Date'])
+    log['month_year'] = pd.to_datetime(log['Date']).dt.to_period('M').astype(str)
+    log['year'] = pd.DatetimeIndex(log['Date']).year
+    log['weekday'] = pd.DatetimeIndex(log['Date']).strftime('%A')
+
+    log.loc[log['Format'] == 'Book', 'pages_norm'] = log['Pages Read']
+    log.loc[log['Format'] == 'eBook', 'pages_norm'] = round(log['Pages Read'] * ebook_normalizer)
+    log.loc[log['Format'] == 'Audiobook', 'pages_norm'] = 0
 
     return books, log
