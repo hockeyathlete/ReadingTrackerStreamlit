b278f23652699fd8029883c0741553f8430b5698
Alan <alan.leeser@gmail.com>
2023-12-31 12:04:29 -0600

    Create main page to load backend data

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pandas as pd
+import streamlit as st
+import backend
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+st.title('Book Tracker')
+st.write(backend.books)
