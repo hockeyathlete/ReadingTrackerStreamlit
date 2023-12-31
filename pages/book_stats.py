0167c81fd0d5909fe09f1d3e0e81a20a74c168bb
Alan <alan.leeser@gmail.com>
2023-12-31 13:37:14 -0600

    Add numpy import Fix data source for books by year chart Move book threshold selection to top (under the title) Add threshold filter to the completed books dataframe Add books by month chart, year selector

--- pages/book_stats.py
+++ pages/book_stats.py
@@ -1,28 +1,51 @@
 import pandas as pd
 import altair as alt
 import streamlit as st
+import numpy as np
+
 
 def books_by_year_chart(books_by_year_data):
-    chart = alt.Chart(books_by_year, title='Books by Year').mark_bar().encode(
+    chart = alt.Chart(books_by_year_data, title='Books by Year').mark_bar().encode(
         x=alt.X('finish_year:O', title='Year'),
         y=alt.Y('Book Title', title=None)
     )
 
     return chart
 
+
+def books_by_month_chart(books_by_month_data):
+    chart = alt.Chart(books_by_month_data, title='Books by Month').mark_bar().encode(
+        x=alt.X('month_year:O', title='Month'),
+        y=alt.Y('Book Title', title=None)
+    )
+
+    return chart
+
+
 st.title('Book Data')
-st.header('Yearly Overview')
 # Threshold to be considered a book
 min_pages_per_book = st.number_input('Pick number of pages to set as threshold', 0)
 
-# filter by finished books
-completed_books = st.session_state.book_data[~st.session_state.book_data['Finish Date'].isnull()]
+st.header('Yearly Overview')
+# filter by finished books (must meet minimum pages threshold to count)
+completed_books = st.session_state.book_data[(~st.session_state.book_data['Finish Date'].isnull()) & (st.session_state.book_data['Pages'] > min_pages_per_book)]
 
-books_by_year = completed_books[completed_books['Pages'] > min_pages_per_book].groupby('finish_year')['Book Title'].count().sort_values(ascending=False).to_frame().reset_index()
-
-
+books_by_year = completed_books.groupby('finish_year')[
+    'Book Title'].count().sort_values(ascending=False).to_frame().reset_index()
 
 st.text('Number of books read each year')
 st.altair_chart(books_by_year_chart(books_by_year))
 
+st.header('Monthly Review')
+# add selection for year to review. Sort the available years in the data.
+# Using list comprehesion, turn all the values into ints because otherwise it adds a decimal point. Then reverse the
+# order of the list to have the most recent year first (this can't be done with the np.sort()
+years = np.sort(completed_books['finish_year'].unique())
+year = st.selectbox('Pick a year to review', [int(year) for year in years][::-1])
+
+year_data = completed_books[completed_books['finish_year'] == year]
+books_by_month = year_data.groupby('month_year')['Book Title'].count().sort_values(
+    ascending=False).to_frame().reset_index()
+
+st.altair_chart(books_by_month_chart(books_by_month))
 st.header('Genres')
