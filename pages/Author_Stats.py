366b3c8fc794cae2ddf7e23f0627510b7a152ee5
Alan <alan.leeser@gmail.com>
2024-02-24 01:16:28 -0600

    refactor keys in Author_Stats.py.py

--- pages/Author_Stats.py
+++ pages/Author_Stats.py
@@ -3,8 +3,8 @@ import altair as alt
 
 def author_chart(author_data):
     chart = alt.Chart(author_data, title='Books by Author').mark_bar().encode(
-        x=alt.X('Author', sort='-y'),
-        y=alt.Y('Book Title', title=None)
+        x=alt.X('author', sort='-y'),
+        y=alt.Y('book_title', title=None)
     )
 
     return chart
@@ -14,7 +14,7 @@ st.title('Author Stats')
 
 
 data = st.session_state.book_data
-books_read = data[data['Status'] == 'Read']
-author_list = books_read.groupby('Author')['Book Title'].count().sort_values(ascending=False).to_frame().reset_index()
+books_read = data[data['status'] == 'Read']
+author_list = books_read.groupby('author')['book_title'].count().sort_values(ascending=False).to_frame().reset_index()
 
 st.altair_chart(author_chart(author_list))
