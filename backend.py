0dc601b1b003c3440dd573d0c46059762069c5ac
Alan <alan.leeser@gmail.com>
2024-02-24 00:53:08 -0600

    Connect to logs database and import as df

--- backend.py
+++ backend.py
@@ -7,18 +7,19 @@ def load_data(user):
     conn = psycopg2.connect(
         "dbname='reading_tracker' user='postgres' password='postgres123' host='localhost' port='5432'")
     books = pd.read_sql('SELECT * FROM books', conn)
+    log = pd.read_sql('SELECT * FROM logs', conn)
     # Load Data
     # if user == 'Alan':
-    sheet_id = '1lQQliuMCJPLcfn3CzaK4aU6-Ro3wkMJ1Ghqa8vdJ1ho'
+    # sheet_id = '1lQQliuMCJPLcfn3CzaK4aU6-Ro3wkMJ1Ghqa8vdJ1ho'
     # elif user == 'Michaels':
     #     sheet_id = '1P6fvMYKev5RS_M3ClTb-KiLPU-HOKplO-3grupqCdNI'
     # elif user == 'Jeff':
     #     sheet_id = '1hJLYGq46kB_Z15HGLy_kCbyB4DLYu2esra_Gl_bFPOU'
     # book_tracker = 'Book%20Tracker'
     # book_tracker_url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={book_tracker}'
-    reading_log = 'Reading%20Log'
-    reading_log_url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={reading_log}'
-    log = pd.read_csv(reading_log_url)
+    # reading_log = 'Reading%20Log'
+    # reading_log_url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={reading_log}'
+    # log = pd.read_csv(reading_log_url)
     ebook_normalizer = 0.89
 
     # Preprocess Book Data
@@ -31,13 +32,13 @@ def load_data(user):
     books.loc[books['format'] == 'Audiobook', 'pages_norm'] = 0
 
     # Preprocess Log Data
-    log['Date'] = pd.to_datetime(log['Date'])
-    log['month_year'] = pd.to_datetime(log['Date']).dt.to_period('M').astype(str)
-    log['year'] = pd.DatetimeIndex(log['Date']).year
-    log['weekday'] = pd.DatetimeIndex(log['Date']).strftime('%A')
+    log['date'] = pd.to_datetime(log['date'])
+    log['month_year'] = pd.to_datetime(log['date']).dt.to_period('M').astype(str)
+    log['year'] = pd.DatetimeIndex(log['date']).year
+    log['weekday'] = pd.DatetimeIndex(log['date']).strftime('%A')
 
-    log.loc[log['Format'] == 'Book', 'pages_norm'] = log['Pages Read']
-    log.loc[log['Format'] == 'eBook', 'pages_norm'] = round(log['Pages Read'] * ebook_normalizer)
-    log.loc[log['Format'] == 'Audiobook', 'pages_norm'] = 0
+    # log.loc[log['format'] == 'Book', 'pages_norm'] = log['Pages Read']
+    # log.loc[log['format'] == 'eBook', 'pages_norm'] = round(log['Pages Read'] * ebook_normalizer)
+    # log.loc[log['format'] == 'Audiobook', 'pages_norm'] = 0
 
     return books, log
